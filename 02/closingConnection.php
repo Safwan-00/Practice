94064da7ec14e56c5ad7ea1dbf5de06a44102112
Safwan <81897224+Supplemental-Farmer@users.noreply.github.com>
2022-02-14 22:54:15 +0600

    ST,CC

--- 02/closingConnection.php
+++ 02/closingConnection.php
@@ -14,6 +14,8 @@
 		$password = ""; 
 		// Create connection 
 		$conn = mysqli_connect($servername, $username, $password); 
+
+		
 		// Check connection 
 		if (!$conn) { 
 			die("Connection failed: " . mysqli_connect_error()); 
