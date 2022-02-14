e7cd93232c487710f3bb0a1260611f8e26167fef
Safwan <81897224+Supplemental-Farmer@users.noreply.github.com>
2022-02-15 03:06:40 +0600

    line 16 in server

--- 02/connectionToDBServer.php
+++ 02/connectionToDBServer.php
@@ -2,24 +2,18 @@
 <html>
 <head>
 	<title>
-		
-
 	</title>
 </head>
 <body>
-
-
 	<?php 
 		$servername = "localhost"; 
 		$username = "root";
 		$password = "";
-		$conn = mysqli_connect($servername, $username, $password); 
-		// Check connection 
+		$conn = mysqli_connect($servername, $username, $password);
 		if (!$conn) { 
 			die("Connection failed: " . mysqli_connect_error()); 
 		} 
 		echo "Connected successfully"; 
 	?>
-
 </body>
 </html>
